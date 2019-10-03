1419c38e03065eb5ed52f643e60854c46faa0c71
Grapeanu Alexandru - Mihai <mihai.grapeanu@lacon.de>
2019-10-03 14:24:54 +0300

    Add data access project to solution

---
+++ WiredBrainCoffee.DataAccess/CoffeeShopDataProvider.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using WiredBrainCoffee.DataAccess.Model;
+
+namespace WiredBrainCoffee.DataAccess
+{
+    public class CoffeeShopDataProvider
+    {
+        public IEnumerable<CoffeeShop> LoadCoffeeShops()
+        {
+            yield return new CoffeeShop { Location = "Frankfurt", BeansInStocInKg = 107 };
+            yield return new CoffeeShop { Location = "Freiburg", BeansInStocInKg = 23 };
+            yield return new CoffeeShop { Location = "Munich", BeansInStocInKg = 56 };
+        }
+    }
+}
